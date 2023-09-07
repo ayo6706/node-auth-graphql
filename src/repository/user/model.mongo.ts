1bbbaf6175b4e92cf23296e16fca3c857761f146
ayo6706 <ayo6706@gmail.com>
2023-09-07 08:42:04 +0100

    implement user repository

---
+++ src/repository/user/model.mongo.ts
@@ -0,0 +1,41 @@
+/* eslint-disable consistent-return */
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import User from "./model";
+import DB_NAMES from "../../helpers/constants";
+
+const schema = new Schema<User>({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: {
+        type: String, required: true, lowercase: true, unique: true,
+    },
+    password: { type: String, required: true }
+}, { timestamps: true });
+schema.pre("save", function save(next) {
+    const user = this;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password!, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+schema.set("toJSON", {
+    transform(doc, ret) {
+        // eslint-disable-next-line no-param-reassign
+        delete ret.password;
+    },
+});
+export default model<User>(DB_NAMES.USER, schema);
---
+++ src/repository/user/user.repository.ts
@@ -0,0 +1,7 @@
+import User from "./model";
+
+export default interface UserRepository {
+    createUser(user: User): Promise<User>
+    findUserByEmail(email: string): Promise<User>;
+    findUserById(id: string): Promise<User>;
+}
